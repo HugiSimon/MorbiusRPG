ecdb0e22907d9d8d09fa73c5b9520e6d007a1e32
HugiSimon <h.simon@ludus-academie.com>
2022-06-01 16:45:55 +0200

    SDL Image 😘 SDL Image fait son entrée, on affiche l'image et on peut la bouger

--- MorbiusRPG/main.cpp
+++ MorbiusRPG/main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <SDL.h>
+#include <SDL_image.h>
 #include <random>
 
 
 int main(int argc, char** argv) {
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stdout, "Échec de l'initialisation de la SDL (%s)\n", SDL_GetError());
+		fprintf(stdout, "Echec de l'initialisation de la SDL (%s)\n", SDL_GetError());
+		return -1;
+	}
+
+	if (IMG_Init(IMG_INIT_JPG) == 0) {
+		fprintf(stdout, "Echec de l'initialisation de la SDL Image (%s)\n", IMG_GetError());
 		return -1;
 	}
 
@@ -21,27 +27,78 @@ int main(int argc, char** argv) {
 
 	renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); 
 
+	int n_avaX = 0;
+	int n_avaY = 0;
+
 	if (pWindow) {
 
 		if (renderer == NULL)
 		{
-			printf("Erreur lors de la creation d'un renderer : %s", SDL_GetError());
+			printf("Erreur lors de la creation d'un renderer (%s)", SDL_GetError());
 			return -1;
 		}
 
+		SDL_Surface* image = IMG_Load("mdr.jpg");
+		if (image == NULL) {
+			fprintf(stdout, "Erreur chargement de l'image (%s)", IMG_GetError());
+			return -1;
+		}
 
-		for (int n_i = 0; n_i < 120; n_i++) {
-			SDL_SetRenderDrawColor(renderer, rand() % 255, rand() % 255, rand() % 255, 255);
+		SDL_Texture* limage = SDL_CreateTextureFromSurface(renderer, image);
+		if (limage == NULL) {
+			fprintf(stdout, "Erreur chargement de la texture (%s)", SDL_GetError());
+			return -1;
+		}
+		SDL_Rect posRec = { 200, 0, 150, 235 };
+
+		while (true) {
+			SDL_Event event;
+			if (SDL_PollEvent(&event)) {
+				if (event.type == SDL_QUIT) {
+					break;
+				}
+
+				if (SDL_KEYDOWN) {
+					switch (event.key.keysym.sym) {
+						case SDLK_UP :
+							n_avaY = -2;
+							break;
+						case SDLK_DOWN :
+							n_avaY = 2;
+							break;
+						case SDLK_LEFT :
+							n_avaX = -2;
+							break;
+						case SDLK_RIGHT :
+							n_avaX = 2;
+							break;
+						default :
+							break;
+					}
+				}
+
+				switch (event.key.state) {
+					case SDL_RELEASED:
+						n_avaX = 0;
+						n_avaY = 0;
+						break;
+					default:
+						break;
+				}
+			}
+
+			posRec.x = posRec.x + n_avaX;
+			posRec.y = posRec.y + n_avaY;
+
 			SDL_RenderClear(renderer);
-			fprintf(stdout, "%d\n", n_i);
+			SDL_RenderCopy(renderer, limage, NULL, &posRec);
 			SDL_RenderPresent(renderer);
+
 			SDL_Delay(16);
 		}
 
-
-		SDL_Delay(3000); //Juste pour le test 
-
-
+		SDL_FreeSurface(image);
+		SDL_DestroyTexture(limage);
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(pWindow);
 
